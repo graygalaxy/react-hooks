d0c28cbaad97cbf548c3b11ccbba49d8810075df
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-08-12 15:03:00 +0530

    :adhesive_bandage: fixed useDelayedRender

--- use-delayed-render.ts
+++ use-delayed-render.ts
@@ -1,55 +1,47 @@
-import { useRef, useCallback } from 'react'
+import { useState, useRef, useCallback } from 'react'
 
 interface Options {
 	enterDelay?: number
 	exitDelay?: number
+	onUnmount?: () => void
 }
 
-/**
- * Hook for delaying render & unmount
- * @param {boolean} active - Render immediately
- * @param {Options} options - Set delay on mount and unmount
- * @returns [isMounted, isRendered]
- * @link https://github.com/pacocoursey/use-delayed-render
- */
-export function useDelayedRender(
-	active: boolean = false,
-	options: Options = {},
-) {
+export function useDelayedRender(active: boolean = false, options: Options = {}) {
 	const mounted = useRef(active)
 	const rendered = useRef(false)
 	const renderTimer = useRef<NodeJS.Timeout | null>(null)
 	const unmountTimer = useRef<NodeJS.Timeout | null>(null)
 	const prevActive = useRef(active)
+	// force to recalculate
+	const [, force] = useState<any>()
 
 	const recalculate = useCallback(() => {
 		const { enterDelay = 1, exitDelay = 0 } = options
 		// component will mount
 		if (prevActive.current) {
+			// Mount Immediately
 			mounted.current = true
-			if (unmountTimer.current) {
-				clearTimeout(unmountTimer.current)
-			}
-			if (enterDelay <= 0) {
-				rendered.current = true
-			} else if (!renderTimer.current) {
+			if (unmountTimer.current) clearTimeout(unmountTimer.current)
+			// Render Immediately
+			if (enterDelay <= 0) rendered.current = true
+			else if (!renderTimer.current) {
 				// Render after a delay
 				renderTimer.current = setTimeout(() => {
 					rendered.current = true
 					renderTimer.current = null
+					force({})
 				}, enterDelay)
 			}
-		}
-		// component will unmount
-		else {
+		} else {
+			// Immediately set to unrendered
 			rendered.current = false
-			if (exitDelay <= 0) {
-				mounted.current = false
-			} else if (!unmountTimer.current) {
+			if (exitDelay <= 0) mounted.current = false
+			else if (!unmountTimer.current) {
 				// Unmount after a delay
 				unmountTimer.current = setTimeout(() => {
 					mounted.current = false
 					unmountTimer.current = null
+					force({})
 				}, exitDelay)
 			}
 		}
